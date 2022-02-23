4705299f8250d63f0b74b2daa550bf26858fb07c
kirinnee <kirinnee97@gmail.com>
2022-02-23 05:12:05 +0000

    feat: added labels for github runners

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -14,5 +14,5 @@ cd actions-runner || exit
 curl -o actions-runner-linux-x64-2.287.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.287.1/actions-runner-linux-x64-2.287.1.tar.gz
 tar xzf ./actions-runner-linux-x64-2.287.1.tar.gz
 rm actions-runner-linux-x64-2.287.1.tar.gz
-./config.sh --url "$url" --token "$token" --name "$full_name"  --unattended --disableupdate --work "_work" --replace
+./config.sh --url "$url" --token "$token" --name "$full_name"  --unattended --disableupdate --work "_work" --replace --labels "docker,ubuntu-20.04,ubuntu-latest,nix"
 tmux new -s "$full_name" -d 'while true; do ./run.sh; done'
