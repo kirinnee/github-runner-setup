fa50806de3752864e11faf6072908bc0250cb552
kirinnee <kirinnee97@gmail.com>
2022-02-23 04:42:43 +0000

    feat: update installer

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -5,6 +5,7 @@ url=$2
 token=$3
 full_name="${HOST}-${name}"
 
+cd "$HOME" || exit
 mkdir actions-runner
 cd actions-runner || exit
 curl -o actions-runner-linux-x64-2.287.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.287.1/actions-runner-linux-x64-2.287.1.tar.gz
