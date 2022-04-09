70c8e1bd4912c3ab9853585b6fee44977396847c
kirinnee <kirinnee97@gmail.com>
2022-04-09 14:34:11 +0000

    feat: updated github runner

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -11,8 +11,8 @@ full_name="${host}-${name}"
 cd "$HOME" || exit
 mkdir actions-runner
 cd actions-runner || exit
-curl -o actions-runner-linux-x64-2.287.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.287.1/actions-runner-linux-x64-2.287.1.tar.gz
-tar xzf ./actions-runner-linux-x64-2.287.1.tar.gz
-rm actions-runner-linux-x64-2.287.1.tar.gz
-./config.sh --url "$url" --token "$token" --name "$full_name"  --unattended --disableupdate --work "_work" --replace --labels "docker,ubuntu-20.04,ubuntu-latest,nix"
+curl -o actions-runner-linux-x64-2.289.2.tar.gz -L https://github.com/actions/runner/releases/download/v2.289.2/actions-runner-linux-x64-2.289.2.tar.gz
+tar xzf ./actions-runner-linux-x64-2.289.2.tar.gz
+rm actions-runner-linux-x64-2.289.2.tar.gz
+./config.sh --url "$url" --token "$token" --name "$full_name"  --unattended --work "_work" --replace --labels "docker,ubuntu-20.04,ubuntu-latest,nix"
 tmux new -s "$full_name" -d 'while true; do ./run.sh; done'
