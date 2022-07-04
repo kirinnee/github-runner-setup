1bdf1c5c85f327c64a875f691bee5dc2d7ca0fd3
kirinnee <kirinnee97@gmail.com>
2022-07-04 16:39:12 +0800

    feat: remove ubuntu and ubuntu latest from tags

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -14,5 +14,5 @@ cd actions-runner || exit
 curl -o actions-runner-linux-x64-2.294.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.294.0/actions-runner-linux-x64-2.294.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.294.0.tar.gz
 rm actions-runner-linux-x64-2.294.0.tar.gz
-./config.sh --url "$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,ubuntu-22.04,ubuntu-latest,nix"
+./config.sh --url "$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix"
 tmux new -s "$full_name" -d 'while true; do ./run.sh; done'
