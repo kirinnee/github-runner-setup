71493042a6bb15c8c3f0ffe370f8d2b448b9c0be
kirinnee <kirinnee97@gmail.com>
2022-08-15 14:58:14 +0800

    feat: update github runner

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -11,9 +11,9 @@ full_name="${host}-${name}"
 cd "$HOME" || exit
 mkdir actions-runner
 cd actions-runner || exit
-curl -o actions-runner-linux-x64-2.294.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.294.0/actions-runner-linux-x64-2.294.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.294.0.tar.gz
-rm actions-runner-linux-x64-2.294.0.tar.gz
+curl -o actions-runner-linux-x64-2.295.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.295.0/actions-runner-linux-x64-2.295.0.tar.gz
+tar xzf ./actions-runner-linux-x64-2.295.0.tar.gz
+rm actions-runner-linux-x64-2.295.0.tar.gz
 ./config.sh --url "$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix"
 sudo ./svc.sh install "$USER"
 sudo ./svc.sh start
