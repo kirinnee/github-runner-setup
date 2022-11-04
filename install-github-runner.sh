51dc54deefb2f027b1fcbb90dd3c747749b6bc6f
kirinnee <kirinnee97@gmail.com>
2022-11-04 07:25:18 +0000

    feat: auto fetch version + multiarch

--- install-github-runner.sh
+++ install-github-runner.sh
@@ -3,17 +3,23 @@
 name=$1
 url=$2
 token=$3
+arch=$4
+
 host="$(hostname)"
 [ "$host" = "" ] && host="random"
-
 full_name="${host}-${name}"
 
 cd "$HOME" || exit
 mkdir actions-runner
+
 cd actions-runner || exit
-curl -o actions-runner-linux-x64-2.295.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.295.0/actions-runner-linux-x64-2.295.0.tar.gz
-tar xzf ./actions-runner-linux-x64-2.295.0.tar.gz
-rm actions-runner-linux-x64-2.295.0.tar.gz
-./config.sh --url "$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix"
+
+version=$(curl --silent "https://api.github.com/repos/actions/runner/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+curl -o "actions-runner.tar.gz" -L "https://github.com/actions/runner/releases/download/$version/actions-runner-linux-$arch-$version.tar.gz"
+
+tar xzf ./actions-runner.tar.gz
+rm ./actions-runner.tar.gz
+./config.sh --url "$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix,$arch"
 sudo ./svc.sh install "$USER"
 sudo ./svc.sh start
