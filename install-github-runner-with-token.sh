9f0d6e75f24b431e03c8c3c01e00bb56179cc235
kirinnee <kirinnee97@gmail.com>
2022-11-09 14:23:48 +0000

    feat: allowed installation with github API token

--- install-github-runner-with-token.sh
+++ install-github-runner-with-token.sh
@@ -14,12 +14,16 @@ mkdir actions-runner
 
 cd actions-runner || exit
 
+# obtain token via GH actions
+jwt=$(curl --request POST "https://api.github.com/repos/${url}/actions/runners/registration-token" --header "Authorization: token ${token}")
+reg_token=$(echo "$jwt" | jq -r '.token')
+
 version=$(curl --silent "https://api.github.com/repos/actions/runner/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 v=$(printf "%s\n" "${version#?}")
 curl -o "actions-runner.tar.gz" -L "https://github.com/actions/runner/releases/download/$version/actions-runner-linux-$arch-$v.tar.gz"
 
 tar xzf ./actions-runner.tar.gz
 rm ./actions-runner.tar.gz
-./config.sh --url "https://github.com/$url" --token "$token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix,$arch"
+./config.sh --url "https://github.com/$url" --token "$reg_token" --name "$full_name" --unattended --work "_work" --replace --labels "docker,nix,$arch"
 sudo ./svc.sh install "$USER"
 sudo ./svc.sh start
